475648dbce879d23df5894305c77b467d80afbac
cotton123236 <cotton123236@gmail.com>
2021-11-06 19:53:31 +0800

    remove gitignore files

--- node_modules/rollup-plugin-sass/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { RollupPluginSassOptions } from "./types";
-import { Plugin as RollupPlugin } from 'rollup';
-declare const _default: (options?: RollupPluginSassOptions) => RollupPlugin;
-export = _default;
--- node_modules/rollup-plugin-sass/dist/utils/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const log: any, warn: any, error: any, isset: (x: any) => boolean, isString: (x: any) => boolean, isObject: (x: any) => boolean, isFunction: (x: any) => boolean;
