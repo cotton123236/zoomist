0e17e4e3832bc1cb6c9b8d4b1c6731470d51af43
Wilson <wilson@begonia-design.com.tw>
2023-10-11 18:33:58 +0800

    build docs

--- docs/src/components/btns/CopyBtn.tsx
+++ docs/src/components/btns/CopyBtn.tsx
@@ -24,7 +24,7 @@ export default ({ children, className, copy }: CopyBtnProps) => {
       className={clsx(
         styles['copy-btn'],
         className,
-        'text-gray-600 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-900'
+        'text-gray-200 hover:bg-gray-50 hover:text-gray-600 dark:text-gray-600 dark:hover:bg-gray-900 dark:hover:text-gray-300'
       )}
       type="button"
       onClick={useCopy}
@@ -32,7 +32,9 @@ export default ({ children, className, copy }: CopyBtnProps) => {
       {children}
       <div className="relative ml-2 h-4 w-4">
         <svg
-          className={clsx('absolute-fill duration-200', { 'opacity-0': isCopied })}
+          className={clsx('absolute-fill transition-opacity duration-200', {
+            'opacity-0': isCopied
+          })}
           fill="none"
           height="24"
           shapeRendering="geometricPrecision"
@@ -44,7 +46,9 @@ export default ({ children, className, copy }: CopyBtnProps) => {
           <path d="M6 17C4.89543 17 4 16.1046 4 15V5C4 3.89543 4.89543 3 6 3H13C13.7403 3 14.3866 3.4022 14.7324 4M11 21H18C19.1046 21 20 20.1046 20 19V9C20 7.89543 19.1046 7 18 7H11C9.89543 7 9 7.89543 9 9V19C9 20.1046 9.89543 21 11 21Z"></path>
         </svg>
         <svg
-          className={clsx('absolute-fill duration-200', { 'opacity-0': !isCopied })}
+          className={clsx('absolute-fill transition-opacity duration-200', {
+            'opacity-0': !isCopied
+          })}
           fill="none"
           height="24"
           shapeRendering="geometricPrecision"
